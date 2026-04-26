a12dd4ff094a573672bcb612018d3479af77df1d
Vivi-invizibl-10 <97378880+Vivi-invizibl-10@users.noreply.github.com>
2026-04-26 11:58:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,36 @@
+const CACHE_NAME = "app-cache-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/pitch_perfect/",
+        "/pitch_perfect/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => {
+        return new Response("You are offline", {
+          headers: { "Content-Type": "text/plain" }
+        });
+      });
+    })
+  );
+});
+
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys.map(k => {
-          if (k !== "app-cache") return caches.delete(k);
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
         })
       );
     })
